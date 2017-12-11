a8b48ea42b609699de96d8863d10b2a490c31921
kwczerwinski <kwczerwinski@outlook.com>
2017-12-11 18:18:28 +0100

    Create class Book.

--- JavaStart_Library/src/program/Book.java
+++ JavaStart_Library/src/program/Book.java
@@ -1,10 +1,36 @@
 package program;
 
 class Book {
-    String title;
-    String author;
-    int releaseDate;
-    int pages;
-    String publisher;
-    String isbn;
+	
+	String title;
+	String author;
+	int releaseDate;
+	int pages;
+	String publisher;
+	String isbn;
+	
+	Book(	String bookTitle,
+			String bookAuthor,
+			int bookRelease,
+			int bookPages,
+			String bookPublisher,
+			String bookIsbn) {
+		title = bookTitle;
+		author = bookAuthor;
+		releaseDate = bookRelease;
+		pages = bookPages;
+		publisher = bookPublisher;
+		isbn = bookIsbn;
+	}
+	
+	void printInfo() {
+		System.out.println("       Tytu³: " + this.title);
+		System.out.println("       Autor: " + this.author);
+		System.out.println("Data wydania: " + this.releaseDate);
+		System.out.println(" Iloœæ stron: " + this.pages);
+		System.out.println("     Wydawca: " + this.publisher);
+		System.out.println("        ISBN: " + this.isbn);
+		System.out.println();
+	}
+	
 }
